c9962986257ac6b00c784abdadbf55e754d5600e
Salvo Nicotra <salvo.nicotra@neodatagroup.ai>
2026-04-14 18:48:59 +0200

    Configuration Management

---
+++ lab/aws-opentofu/network.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "ssh" {
+  name        = "lab-ssh-sg"
+  description = "Allow SSH inbound"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
